06ed9d3fec5d003ee89a2693d94c793cfe2cc028
John Martin <curmudgeon@rallydev.com>
2013-04-18 19:25:47 -0700

    choose range of sprints

--- App.js
+++ App.js
@@ -2,68 +2,174 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     defaults: { padding: 5 },
+    allRelease: "--ANY--",
     items: [
-        {xtype:'container',itemId:'selector_box'},
+        {xtype:'container',itemId:'selector_box', layout: { type: 'hbox' }},
         {xtype:'container',itemId:'average_box'},
         {xtype:'container',itemId:'chart_box'}
     ],
     launch: function() {
         this._addSelectors();
-        this._getIterations();
     },
     number_of_iterations: 5,
     _addSelectors: function() {
+        var me = this;
+        var first_time = true;
         this.down('#selector_box').add({
-            xtype: 'rallynumberfield',
-            value: 5,
-            fieldLabel: 'Number of Sprints',
-            validator: function(value) {
-                if ( value < 1 ) {
-                    return "Number of Iterations cannot be less than 1";
+            xtype:'rallyreleasecombobox',
+            itemId:'release_box',
+            width: 300,
+            fieldLabel: "Release Window:",
+            listeners: {
+                change: function(rb,newValue,oldValue) {
+                    this._updateIterationBoxes(rb.getRecord());
+                },
+                ready: function(rb) {
+                    this._updateIterationBoxes(rb.getRecord());
+                },
+                scope: this
+            },
+            storeConfig: {
+                listeners: {
+                    load: function(store) {
+                        if ( first_time ) {
+                            store.loadData([{formattedName: me.allRelease,
+                                            formattedStartDate: 'n/a',
+                                            formattedEndDate: 'n/a',
+                                            Name: me.allRelease,
+                                            isSelected: false}],
+                                            true);
+                            store.sort('formattedStartDate', 'DESC');
+                            first_time = false;
+                        }
+                     }
                 }
-                return true;
+            }
+        });
+    },
+    _updateIterationBoxes: function(release) {
+        window.console && console.log("_updateIterationBoxes",release);
+        var me = this;
+        if ( this.chart ) { this.chart.destroy(); }
+        this.down('#average_box').removeAll(true);
+        if ( this.start_box ) { this.start_box.destroy(); }
+        if ( this.end_box ) { this.end_box.destroy(); }
+        if ( this.down('#calculate_button') ) { this.down('#calculate_button').destroy(); }
+        var release_filter = [
+            {property:'EndDate',operator:'>=',value:Rally.util.DateTime.toIsoString(release.get('ReleaseStartDate'))},
+            {property:'StartDate',operator:'<=',value:Rally.util.DateTime.toIsoString(release.get('ReleaseDate'))}
+        ];
+
+        this.start_box = Ext.create('Rally.ui.combobox.IterationComboBox',{
+            width: 300,
+            itemId:'start_iteration_box',
+            fieldLabel: 'Start Iteration:',
+            storeConfig: {
+                filters: release_filter
             },
             listeners: {
-                change: function(field,new_value) {
-                    if ( this.chart ){ this.chart.destroy(); }
-                    this.number_of_iterations = new_value;
-                    this._getIterations();
+                ready: function(ib) {
+                    if (ib.getStore().getAt(0)) {
+                        ib.setValue(ib.getStore().getAt(ib.getStore().getCount()-1).get('_ref'));
+                    }
+                    me.end_box = Ext.create('Rally.ui.combobox.IterationComboBox',{
+                        width: 300,
+                        itemId:'end_iteration_box',
+                        fieldLabel: 'End Iteration:',
+                        storeConfig: {
+                            filters: release_filter
+                        },
+                        listeners: {
+                            ready: function(ib) {
+                                if (ib.getStore().getAt(0)) {
+                                    ib.setValue(ib.getStore().getAt(0).get('_ref'));
+                                }
+                                
+                                me._setButton();
+                            },
+                            change: function(ib){
+                                me.down('#average_box').removeAll(true);
+                                if ( me.chart ) { me.chart.destroy(); }
+                                me._setButton();
+                            }
+                        }
+                    });
+                    me.down('#selector_box').add(me.end_box);
+                    me.down('#selector_box').add({
+                        itemId:'calculate_button',
+                        xtype:'rallybutton',
+                        text:'Display Velocities',
+                        disabled:true,
+                        handler: function() {
+                            me._getIterations();
+                        }
+                    });
+                },
+                change: function(ib){
+                    me.down('#average_box').removeAll(true);
+                    if ( me.chart ) { me.chart.destroy(); }
+                    me._setButton();
+                }
+            }
+        });
+        this.down('#selector_box').add(this.start_box);
+    },
+    _setButton: function() {
+        window.console && console.log("_setButton");
+        if ( this.down('#calculate_button') ) {
+            this.down('#calculate_button').setDisabled(true);
+            if ( this.end_box && this.start_box ) {
+                var start_sprint = this.start_box.getRecord();
+                var end_sprint = this.end_box.getRecord();
+                if ( start_sprint && end_sprint ) {
+                    if ( start_sprint.get('StartDate') <= end_sprint.get('StartDate') ) {
+                        this.down('#calculate_button').setDisabled(false);
+                    }
+                }
+            }
+        }
+    },
+    _getIterations: function() {
+        window.console && console.log("_getIterations");
+        var me = this;
+        
+        var start_date = Rally.util.DateTime.toIsoString(this.start_box.getRecord().get('StartDate'));
+        var end_date = Rally.util.DateTime.toIsoString(this.end_box.getRecord().get('EndDate'));
+        var today = Rally.util.DateTime.toIsoString(new Date());
+        
+        if ( start_date < today ) {
+            if ( today < end_date ) {
+                end_date = today;
+            }
+        }
+        
+        var filters = [
+            {property:'EndDate',operator:'>=',value:start_date},
+            {property:'StartDate',operator:'<=',value:end_date}
+        ];
+        this.sprint_hash = {};
+        Ext.create('Rally.data.WsapiDataStore',{
+            model: 'Iteration',
+            /*limit: this.number_of_iterations,*/
+            autoLoad: true,
+            fetch:['Name'],
+            context: {projectScopeUp:false,projectScopeDown:false},
+            filters: filters,
+            sorters: [{property:'EndDate', direction: 'DESC'}],
+            listeners: {
+                load: function(store,data,success){
+                    Ext.Array.each( data, function(item){
+                        me.sprint_hash[item.get('Name')] = null;
+                    }, this, true);
+                    for (var name in me.sprint_hash) {
+                        if ( me.sprint_hash.hasOwnProperty(name) ) {
+                            this._getStoriesForSprint(name);
+                        }
+                    }
                 },
                 scope: this
             }
         });
-    },
-    _getIterations: function() {
-        window.console && console.log("_getIterations",this.number_of_iterations);
-        var me = this;
-        this.down('#average_box').removeAll(true);
-        
-        this.sprint_hash = {};
-        if ( this.number_of_iterations > 0 ) {
-            Ext.create('Rally.data.WsapiDataStore',{
-                model: 'Iteration',
-                limit: this.number_of_iterations,
-                pageSize: this.number_of_iterations,
-                autoLoad: true,
-                fetch:['Name'],
-                context: {projectScopeUp:false,projectScopeDown:false},
-                filters: [{property:'EndDate',operator:'<',value:Rally.util.DateTime.toIsoString(new Date())}],
-                sorters: [{property:'EndDate', direction: 'DESC'}],
-                listeners: {
-                    load: function(store,data,success){
-                        Ext.Array.each( data, function(item){
-                            me.sprint_hash[item.get('Name')] = null;
-                        }, this, true);
-                        for (var name in me.sprint_hash) {
-                            if ( me.sprint_hash.hasOwnProperty(name) ) {
-                                this._getStoriesForSprint(name);
-                            }
-                        }
-                    },
-                    scope: this
-                }
-            });
-        }
     },
     _getStoriesForSprint:function(name){
         window.console && console.log('_getStoriesForSprint',name);
@@ -151,7 +257,6 @@ Ext.define('CustomApp', {
             }
         });
         velocity_array = velocity_array.sort();
-        console.log(velocity_array);
         
         if (velocity_total > 0 ) {
             var average = this._formatFewerDecimals(velocity_total / velocity_array.length);
